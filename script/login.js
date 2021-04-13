54401968d40e85d287a5b58745b58722d9993b68
Sulash manandhar <np03a190033@heraldcollege.edu.np>
2021-04-13 11:20:17 +0545

    initial commit

---
+++ script/login.js
@@ -0,0 +1,22 @@
+console.log("Hello");
+localStorage.clear();
+localStorage.setItem("email", "adminLogin@heraldcollege.edu.np");
+localStorage.setItem("password", "adminLogin");
+
+
+var inputEmail =document.getElementById("email-input");
+var inputPassword= document.getElementById("password-input");
+var btnInput=document.getElementById("btn");
+
+
+var email=localStorage.getItem("email");
+var password=localStorage.getItem("password");
+console.log(email,password);
+
+function myFunction() {
+    const key=inputEmail;
+    const val=inputPassword;
+    console.log(key);
+    console.log(val);
+    
+}
