2141eab8e303111d1aed3721f352da75a368f1e5
Sulash Manandhar <np03a190033@heraldcollege.edu.np>
2021-05-09 13:42:29 +0545

    index page done, watch-us incomplete

--- react-app/src/pages/index.js
+++ react-app/src/pages/index.js
@@ -1,11 +1,46 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import axios from 'axios';
+import '../stylesheet/index.css'
 
 
 export default class Index extends Component {
+    constructor(props){
+        super(props);
+        this.state={
+            data:[],
+        }
+    }
+    componentDidMount(){
+        axios.get('http://localhost:5000/index-contain')
+        .then(res => {
+            this.setState({
+                data: res.data
+            })
+        })
+        .catch(error=>{
+            console.log("Error!!!");
+        })
+    }
     render() {
         return (
             <>
-                <h1>This is index page.</h1>
+                <div id="hero-img">
+                    <img src="../images/hero-img1.png" alt="hero-img" width="100%" height="auto"/>
+                </div>
+                <div className="main-wrapper clearfix">
+                    {this.state.data.map(item=>(
+                        <div className="container">
+                            <div class="img-container clearfix">
+                                <img src={item.image} alt="demo-img"/>
+                            </div>
+                            <div className="text-container clearfix">
+                                <h1>{item.heading}</h1>
+                                <p>{item.paragraph}</p>
+                            </div>
+                    
+                        </div>
+                    ))}
+                </div>
             </>
         )
     }
--- react-app/src/pages/watch-us.js
+++ react-app/src/pages/watch-us.js
@@ -1,11 +1,54 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import axios from 'axios';
+import '../stylesheet/watch-us.css'
+
 
 export default class WatchUs extends Component {
+    constructor(props){
+        super(props);
+        this.state={
+            data:[],
+        }
+    }
+    componentDidMount(){
+        axios.get('http://localhost:5000/watch-us')
+        .then(res=>{
+            this.setState({
+                data:res.data
+            })
+        })
+        .catch(error=>{
+            console.log("Error!!!");
+        })
+    }
     render() {
         return (
-            <div>
-                <h1>This is watch us</h1>
+            <>
+            {/* hero-video  */}
+            <div className="video-container">
+            <video width="1200px" controls>
+                <source src="../video/Herald College TVC.mp4" type="video/mp4"/>
+            </video>
             </div>
+
+            <h1><u>More video about us..</u></h1>
+            <div className="main-container clearfix">
+               {this.state.data.map(item=>(
+                   <div className="sub-container clearfix">
+                        <div className="img-container">
+                            <img src={item.image} alt="img" />
+                        </div>
+                        <div className="text-container">
+                            <h2>{item.heading}</h2>
+                            <a href={item.link}>{item.link}</a>
+                            <p>Description:{item.description}</p>
+                        </div>
+                   </div>
+               ))}
+            </div>
+
+
+           </>
         )
     }
 }
