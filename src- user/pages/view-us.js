be1b6ea362101c3c4f3a3fc55f110f86034b7e1a
Rishab Sharma <np03a190051@heraldcollege.edu.np>
2021-05-13 15:25:42 +0545

    view us and contact us added

--- src- user/pages/view-us.js
+++ src- user/pages/view-us.js
@@ -1,62 +1,39 @@
 import React, { Component } from 'react'
 import '../stylesheet/view-us.css';
+import axios from 'axios';
 export default class ViewUs extends Component {
+  constructor(props){
+    super(props);
+    this.state={
+        data:[],
+    }
+}
+componentDidMount(){
+    axios.get('http://localhost:5000/view-us')
+    .then(res=>{
+        this.setState({
+            data:res.data
+        })
+    })
+    .catch(error=>{
+        console.log("Error!!!");
+    })
+}
     render() {
         return (
          <>
          <div className="gallery-section">
       <div class="gallery-inner-width">
-        <h1>Herald COllege Gallery</h1>
+        <h1>Herald College Gallery</h1>
         <div className="gallery-border"></div>
         <div className="gallery">
 
-          <a href="image/1.jpg" className="image">
-            <img src="../images/Photo.jpg" alt=""/>
-          </a>
 
-          <a href="image/2.jpg" className="image">
-            <img src="../images/Photo1.jpg" alt=""/>
+        {this.state.data.map(item=>(
+          <a href={item.image} className="image">
+            <img src={item.image} alt={item.description}/>
           </a>
-
-          <a href="image/3.jpg" className="image">
-            <img src="../images/Photo2.jpg" alt=""/>
-          </a>
-
-          <a href="image/4.jpg" className="image">
-            <img src="../images/Photo3.jpg" alt=""/>
-          </a>
-
-          <a href="image/5.jpg" className="image">
-            <img src="../images/Photo4.jpg" alt=""/>
-          </a>
-
-          <a href="image/6.jpg" className="image">
-            <img src="../images/Photo5.jpg" alt=""/>
-          </a>
-
-          <a href="image/7.jpg" className="image">
-            <img src="../images/Photo6.jpg" alt=""/>
-          </a>
-
-          <a href="image/8.jpg" className="image">
-            <img src="../images/Photo7.jpg" alt=""/>
-          </a>
-
-          <a href="image/8.jpg" className="image">
-            <img src="../images/Photo8.jpg" alt=""/>
-          </a>
-
-          <a href="image/8.jpg" className="image">
-            <img src="../images/Photo9.jpg" alt=""/>
-          </a>
-
-          <a href="image/8.jpg" className="image">
-            <img src="../images/Photo10.jpg" alt=""/>
-          </a>
-
-          <a href="image/8.jpg" className="image">
-            <img src="../images/Photo11.jpg" alt=""/>
-          </a>
+        ))}
          
         </div>
       </div>
