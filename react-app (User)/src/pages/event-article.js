d7911c63ef9ac06e3086c3b1475fa51ede1b563a
Sulash Manandhar <np03a190033@heraldcollege.edu.np>
2021-05-14 14:17:07 +0545

    view us and event page added; modified version

--- react-app (User)/src/pages/event-article.js
+++ react-app (User)/src/pages/event-article.js
@@ -1,10 +1,49 @@
 import React, { Component } from 'react'
+import '../stylesheet/event-article.css';
+import axios from 'axios';
+
+const textCenter={
+    textAlign:'center',
+}
 
 export default class EventArticle extends Component {
+    constructor(props){
+        super(props);
+        this.state={
+            data:[],
+        }
+    }
+    componentDidMount(){
+        axios.get('http://localhost:5000/event-and-article')
+        .then(res=>{
+            this.setState({
+                data:res.data
+            })
+        })
+        .catch(error=>{
+            console.log("Error!!!");
+        })
+    }
     render() {
         return (
             <div>
-                 <h1>This is EventArticle page.</h1>
+                <div className="event-hero-img">
+                    <img src="../images/Herald_College_3.jpg" width="100%" alt="hero-img"/>
+                </div>
+                {this.state.data.map(item=>(
+                      <div className="event-column">
+                      <div class="event-title">
+                          <h2 className="text-center">{item.title}</h2>
+                          <span>{item.type}</span>
+                      </div>
+
+                      <p>{item.line1}</p>
+                      <p>{item.line2}</p>
+                      <p>{item.line3}</p>
+                      <p>{item.line4}</p>
+                      <p>{item.line5}</p>
+                  </div>
+                ))}
             </div>
         )
     }
--- react-app (User)/src/pages/view-us.js
+++ react-app (User)/src/pages/view-us.js
@@ -1,11 +1,44 @@
 import React, { Component } from 'react'
-
+import '../stylesheet/view-us.css';
+import axios from 'axios';
 export default class ViewUs extends Component {
+  constructor(props){
+    super(props);
+    this.state={
+        data:[],
+    }
+}
+componentDidMount(){
+    axios.get('http://localhost:5000/view-us')
+    .then(res=>{
+        this.setState({
+            data:res.data
+        })
+    })
+    .catch(error=>{
+        console.log("Error!!!");
+    })
+}
     render() {
         return (
-            <div>
-                 <h1>This is view us page.</h1>
-            </div>
+         <>
+         <div className="gallery-section">
+      <div class="gallery-inner-width">
+        <h1>Herald College Gallery</h1>
+        <div className="gallery-border"></div>
+        <div className="gallery">
+
+
+        {this.state.data.map(item=>(
+          <a href={item.image} className="image">
+            <img src={item.image} alt={item.description}/>
+          </a>
+        ))}
+         
+        </div>
+      </div>
+    </div>
+         </>
         )
     }
 }
